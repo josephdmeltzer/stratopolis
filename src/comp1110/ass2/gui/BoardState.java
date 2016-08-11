f544e16ac9a1c241739123911329e6246e6c4b5d
u5807060 <u5807060@anu.edu.au>
2016-08-11 14:15:31 +1000

    Tiles, Colour, BoardState

---
+++ src/comp1110/ass2/gui/BoardState.java
@@ -0,0 +1,9 @@
+package comp1110.ass2.gui;
+
+/**
+ * Created by Aftran261 on 11/08/2016.
+ */
+public class BoardState {
+    public Colour playerTurn;
+    public Tiles[] all_the_tiles; /*this might not be implemented as an array in the end*/
+}
---
+++ src/comp1110/ass2/gui/Colour.java
@@ -0,0 +1,6 @@
+package comp1110.ass2.gui;
+
+/**
+ * Created by Aftran261 on 11/08/2016.
+ */
+public enum Colour {RED,GREEN,COMPSTART}
