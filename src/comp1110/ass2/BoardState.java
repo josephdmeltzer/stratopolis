9c58258bc509c939f07c6d9bd671060042448198
u5807060 <u5807060@anu.edu.au>
2016-08-11 14:18:58 +1000

    some comments, and putting files outside GUI

--- src/comp1110/ass2/BoardState.java
+++ src/comp1110/ass2/BoardState.java
@@ -1,8 +1,9 @@
-package comp1110.ass2.gui;
+package comp1110.ass2;
 
 /**
  * Created by Aftran261 on 11/08/2016.
  */
+/*This describes the entire board, like in Kalaha.*/
 public class BoardState {
     public Colour playerTurn;
     public Tiles[] all_the_tiles; /*this might not be implemented as an array in the end*/
