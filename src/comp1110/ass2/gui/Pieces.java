85a90297fccf2277378a9ae47a8bb4b7619aa58f
manalmohania <u6019071@anu.edu.au>
2016-08-11 14:10:32 +1000

    Add Pieces enum

--- src/comp1110/ass2/gui/Pieces.java
+++
@@ -1,9 +0,0 @@
-package comp1110.ass2.gui;
-
-/**
- * Created by manalmohania on 11/08/2016.
- */
-
-/*This class contains a representation of the pieces and functions associated to it, if any*/
-public enum Pieces {
-}
