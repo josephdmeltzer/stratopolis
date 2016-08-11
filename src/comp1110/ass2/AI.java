1fe8a02e37106ff99c7b2b76587c49479d4cdcdb
Joseph Dylan Meltzer <u6060661@anu.edu.au>
2016-08-11 16:15:45 +1200

    Task 1: Added AI class, and import to gui.Board

---
+++ src/comp1110/ass2/AI.java
@@ -0,0 +1,11 @@
+package comp1110.ass2;
+
+/**
+ * Created by josephmeltzer on 11/08/16.
+ */
+public class AI {
+    /**
+     * This class will contain methods that an AI player will use
+     * to make piece placement decisions.
+     */
+}
