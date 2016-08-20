a7f6e7abd626c707c2b2e0090043f83051f89bb8
manalmohania <u6019071@anu.edu.au>
2016-08-21 00:27:43 +1000

    Representation for tiles -- going off to sleep now

--- src/comp1110/ass2/Pieces.java
+++ src/comp1110/ass2/Pieces.java
@@ -1,9 +1,41 @@
 package comp1110.ass2;
 
+import static comp1110.ass2.Colour.*;
+
+
 /**
  * Created by manalmohania on 11/08/2016.
  */
 
 /*This enum contains a representation of the pieces and any associated functions*/
 public enum Pieces {
+    A (RED, BLACK, BLACK),
+    B (BLACK, BLACK, RED),
+    C (BLACK, RED, BLACK),
+    D (RED, BLACK, RED),
+    E (BLACK, RED, RED),
+    F (RED, RED, BLACK),
+    G (RED, GREEN, RED),
+    H (GREEN, RED, RED),
+    I (RED, RED, GREEN),
+    J (RED, RED, RED),
+    K (GREEN, BLACK, BLACK),
+    L (BLACK, BLACK, GREEN),
+    M (BLACK, GREEN, BLACK),
+    N (GREEN, BLACK, GREEN),
+    O (BLACK, GREEN, GREEN),
+    P (GREEN, GREEN, BLACK),
+    Q (GREEN, RED, GREEN),
+    R (RED, GREEN, GREEN),
+    S (GREEN, GREEN, RED),
+    T (GREEN, GREEN, GREEN);
+
+    final Colour[] colours = new Colour[3];
+
+    Pieces(Colour a, Colour b, Colour c){
+       // colours = new Colour[3];
+        colours[0] = a;
+        colours[1] = b;
+        colours[2] = c;
+    }
 }
