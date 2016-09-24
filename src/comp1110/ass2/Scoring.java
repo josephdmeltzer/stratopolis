80080f6710173afb925e63d3fa95ebbaba6002b3
manalmohania <u6019071@anu.edu.au>
2016-09-24 11:17:37 +1000

    Added comments to the scoring class

--- src/comp1110/ass2/Scoring.java
+++ src/comp1110/ass2/Scoring.java
@@ -11,10 +11,12 @@ import static comp1110.ass2.Colour.*;
  * Created by manalmohania on 25/08/2016.
  */
 
-/*Idea for and implementation of class by Manal Mohania*/
+/**
+ * This class contains methods relating to scoring and winner determination.
+ * All methods implemented by Manal Mohania.
+ * */
 
 public final class Scoring {
-    /*I wanted to have a separate class to handle scoring -- Manal*/
     private static final int BOARD_SIZE = 26;
 
     /* The following declarations are global for efficiency purposes only
@@ -26,20 +28,25 @@ public final class Scoring {
     private static int[][] candidates = new int[400][2]; // An upper bound for the number of contiguous regions of a certain colour
     private static boolean winnerByChance; // If this bool is true, you'll know that the winner has been determined by chance.
 
-    /*returns the winner. `true` denotes that green is the winner.*/
+    /**
+     * Given a placement string, this method returns the winner according to the rules of the game.
+     *
+     * @param placement: a valid placement string (of max length)
+     * @return the winner as a boolean - true means that green has won and false that red is the winner.
+     * */
     public static boolean getWinner(String placement) {
 
         winnerByChance = false;
         int[][] greenStuff = new int[400][2];
         int[][] redStuff = new int[400][2];
+
         /*1. call getScore for green
         * 2. have a copy of the candidates
         * 3. call getScore fo red, if they're not equal, well and good. Bye bye.
         * 4. else have a copy of the candidates for red.
         * 5. Remove those entries, shift each valid entry to the left by one
         * 6. Repeat processes above.
-        * 7. I'd rather have a method that identifies the regions - will saves tons of code
-        * 8. Base case - rand value*/
+        * 7. Base case - rand value*/
         int greenScore = getScore(placement, true);
 
         for (int i = 0; i < 400; i++) {
@@ -70,6 +77,15 @@ public final class Scoring {
 
         return nextResult(redStuff, greenStuff);
     }
+
+    /**
+     * This function is used only when there has been a tie due to equal scores.
+     * In case there is a tie again, based on the second largest areas, this function recursively calls itself until a winner is decided
+     *
+     * @param red: a 2d array containing the area of the width of contiguous regions and the max height in it for the red player
+     * @param green: a 2d array containing the area of the width of contiguous regions and the max height in it for the green player
+     *
+     * @return the winner based on the boolean values as described above*/
 
     private static boolean nextResult(int[][] red, int[][] green) {
         /*identify max*/
@@ -160,7 +176,13 @@ public final class Scoring {
         return nextResult(red, green);
     }
 
-    public static int getScore(String placement, boolean green) {
+    /**given a placement string, this function returns the score based on the rules of the game
+     *
+     * @param placement: A valid placement string
+     * @param green: A booleean value denoting if it's green's turn
+     *
+     * @return the score*/
+    static int getScore(String placement, boolean green) {
 
         colours = colourArray(placement);
         colours2 = colourArray(placement);
@@ -226,6 +248,15 @@ public final class Scoring {
         return maxArea * maxHeight;
     }
 
+    /**
+     * This function calculates the maximum height in a contiguous region given the coordinates of any one cell on it
+     *
+     * @param col: The column value of the cell of which the height is required
+     * @param row: The rwo value of the cell of which the height is required
+     * @param colour: The colour of the cell of which the height is being evaluated
+     * @param max: The maximum height calculated in the region so far
+     *
+     * @return the max height in the region*/
     private static int floodHeight(int col, int row, Colour colour, int max) {
 
         int val;
@@ -244,6 +275,14 @@ public final class Scoring {
         return myMax(floodHeight(col + 1, row, colour, val), floodHeight(col - 1, row, colour, val), floodHeight(col, row + 1, colour, val), floodHeight(col, row - 1, colour, val));
     }
 
+    /**
+     * This function calculates the are of a contiguous region of one colour given the coordinates of any one cell on it
+     *
+     * @param col: The column value of the cell for which the area is required
+     * @param row: The rwo value of the cell of for which the area is required
+     * @param colour: The colour of the cell of for which the area being evaluated
+     *
+     * @return the area of the region*/
     private static int floodFill(int col, int row, Colour colour) {
 
         int val;
@@ -267,17 +306,22 @@ public final class Scoring {
         return val;
     }
 
+    /**Calculates the maximum of four intgers
+     * @params a, b, c, d: the four integers
+     * @return the max of the four
+     * */
     private static int myMax(int a, int b, int c, int d) {
         return Math.max(Math.max(Math.max(a, b), c), d);
     }
 
-    /*This method hasn't been called as yet, because it will only be used in the very end*/
+    /**
+     * The method checks if the winner has been determined by chance
+     *
+     * @return a boolean, true if the winner has been determined by chance
+     *
+     * NOTE: This method hasn't been called as yet, because it will only be used in the very end*/
     static boolean isWinnerByChance() {
         return winnerByChance;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(getScore("MMUALLNA", true));
     }
 
 }
