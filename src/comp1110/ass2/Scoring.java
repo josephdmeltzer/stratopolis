5efe99f3f876950228df0b53eaf6a50b4ca64375
manalmohania <u6019071@anu.edu.au>
2016-09-25 10:50:41 +1000

    Fixed bug in scoring

--- src/comp1110/ass2/Scoring.java
+++ src/comp1110/ass2/Scoring.java
@@ -21,7 +21,7 @@ public final class Scoring {
 
     /* The following declarations are global for efficiency purposes only
     *  Do NOT add any methods to try and access/write to them. */
-    private static int[][] flags = new int[BOARD_SIZE][BOARD_SIZE];
+    /*private static int[][] flags = new int[BOARD_SIZE][BOARD_SIZE];*/
     private static Colour[][] colours;
     private static Colour[][] colours2;
     private static int[][] heights = new int[BOARD_SIZE][BOARD_SIZE];
@@ -194,12 +194,13 @@ public final class Scoring {
             }
         }*/
 
-        /*reset all flags to zero*/
+        /*
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
                 flags[i][j] = 0;
             }
         }
+        */
         int k = 0;
 
         /*reset candidate values to 0*/
@@ -208,19 +209,21 @@ public final class Scoring {
             candidates[i][1] = 0;
         }
 
+        /*
         flags[12][12] = 1;
         flags[12][13] = 1;
+        */
 
         // check if colour is the required one and corresponding flag is 0. If yes, proceed to find the corresponding score
 
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-                if (colours[i][j] == GREEN && flags[i][j] == 0 && green) {
+                if (colours[i][j] == GREEN && /*flags[i][j] == 0 && */green) {
                     int val = floodFill(i, j, GREEN);
                     candidates[k][0] = val;
                     candidates[k][1] = floodHeight(i, j, GREEN, 0);
                     k++;
-                } else if (!green && colours[i][j] == RED && flags[i][j] == 0) {
+                } else if (!green && colours[i][j] == RED /*&& flags[i][j] == 0*/) {
                     int val = floodFill(i, j, RED);
                     candidates[k][0] = val;
                     candidates[k][1] = floodHeight(i, j, RED, 0);
@@ -233,9 +236,9 @@ public final class Scoring {
         int maxHeight = 1;
 
         for (int i = 0; i < 400; i++) {
+            // System.out.println(i + "\t" + candidates[i][0] + "\t" + candidates[i][1]);
             if (candidates[i][0] == 0)
                 break;
-            // System.out.println(i + "\t" + candidates[i][0] + "\t" + candidates[i][1]);
             maxArea = (candidates[i][0] > maxArea ? candidates[i][0] : maxArea);
         }
 
@@ -291,7 +294,7 @@ public final class Scoring {
             return 0;
         }
 
-        flags[col][row] = 1;
+        /*flags[col][row] = 1;*/
 
         if (colours[col][row] != colour) {
             return 0;
@@ -322,6 +325,10 @@ public final class Scoring {
      * NOTE: This method hasn't been called as yet, because it will only be used in the very end*/
     static boolean isWinnerByChance() {
         return winnerByChance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getScore("MMUANMTAMNCD", false));
     }
 
 }
