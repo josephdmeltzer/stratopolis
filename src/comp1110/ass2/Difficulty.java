b66827e8dd20c83d6343a23224a2a9ba64615b70
u5807060 <u5807060@anu.edu.au>
2016-09-29 19:40:42 +1000

    made 4x4 playing modes, changed game over screen

---
+++ src/comp1110/ass2/Difficulty.java
@@ -0,0 +1,6 @@
+package comp1110.ass2;
+
+/**
+ * Created by Aftran261 on 29/09/2016.
+ */
+public enum Difficulty {HUMAN,EASY,MEDIUM,HARD,CHEATING}
--- src/comp1110/ass2/GameState.java
+++ src/comp1110/ass2/GameState.java
@@ -4,18 +4,20 @@ package comp1110.ass2;
  * Created by u5807060 on 11/08/2016.
  */
 /*Implemented by Zhixian Wu*/
-public class BoardState {
+public class GameState {
     public Colour playerTurn; /*Who has the current turn*/
-    public PlayingMode playingMode; /*If the game is two-player,
-          one-player with you playing as red, or one-player with you playing as green*/
+    public Difficulty greenPlayer;
+    public Difficulty redPlayer;
     public String moveHistory; /*The moves made so far*/
+
 
     /*The class constructor
     * @param colour    Who has the current turn (playerTurn)
     * @param mode      playingMode*/
-    public BoardState(Colour colour, PlayingMode mode){
+    public GameState(Colour colour, Difficulty diffG, Difficulty diffR){
         this.playerTurn = colour;
-        this.playingMode = mode;
+        this.greenPlayer = diffG;
+        this.redPlayer = diffR;
         this.moveHistory = "";
     }
 
