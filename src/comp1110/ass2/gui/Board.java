584724d2c37ca72a9f1a90e7bf0ac1e10c79304e
Joseph Meltzer <u6060661@anu.edu.au>
2016-09-29 15:16:21 +1000

    AI now only searches through the set of moves that are adjacent or on top of currently placed tile. Much faster! Almost playable at the beginning of the game.

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -138,7 +138,7 @@ public class Board extends Application {
             char greenTile = (char) (playerG.available_tiles).get(playerG.used_tiles);
             String opponent;
             /* Condition for using the probabilistic AI. pAI is only really playable for the last 1 move. */
-            if (AI.piecesLeft(boardState.moveHistory, true).size() <= 1) {
+            if (AI.piecesLeft(boardState.moveHistory, true).size() >= 0) {
                 opponent = generateMove(boardState.moveHistory, greenTile, redTile);
             }
             else {
@@ -636,7 +636,7 @@ public class Board extends Application {
                 char greenTile = (char) (playerG.available_tiles).get(playerG.used_tiles);
                 String opponent;
                 /* Joseph Meltzer: Condition for using the probabilistic AI. AI is only really playable for the last 1 move. */
-                if (AI.piecesLeft(boardState.moveHistory, false).size() <= 1) {
+                if (AI.piecesLeft(boardState.moveHistory, false).size() >= 0) {
                     opponent = generateMove(boardState.moveHistory, redTile, greenTile);
                 }
                 else {
@@ -714,7 +714,7 @@ public class Board extends Application {
                 char greenTile = (char) (playerG.available_tiles).get(playerG.used_tiles);
                 String opponent;
                 /* Condition for using the probabilistic AI. pAI is only really playable for the last 1 move. */
-                if (AI.piecesLeft(boardState.moveHistory, true).size() <= 1) {
+                if (AI.piecesLeft(boardState.moveHistory, true).size() >= 0) {
                     opponent = generateMove(boardState.moveHistory, greenTile, redTile);
                 }
                 else {
