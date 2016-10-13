8fea1c2ebe28f0870f6573c42ef1bbfac2a8c778
Joseph Meltzer <u6060661@anu.edu.au>
2016-10-13 22:59:33 +1100

    Winning text formatting

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1345,9 +1345,9 @@ layout by Manal Mohania and Joseph Meltzer*/
                 /*If green wins*/
                 if (Scoring.getWinner(gameState.moveHistory)){
                     Text score = new Text("Green Wins!");
-                    score.setStyle("-fx-stroke: black; -fx-stroke-width: 2");
+                    score.setStyle("-fx-stroke: black; -fx-stroke-width: 1");
                     score.setFill(Color.GREEN);
-                    score.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+                    score.setFont(Font.font("Arial", FontWeight.BOLD, 36));
                     popUp2.getChildren().clear(); /*get rid of the last game's result*/
                     popUp2.getChildren().add(score);
                     score.setLayoutX(280);
@@ -1356,8 +1356,8 @@ layout by Manal Mohania and Joseph Meltzer*/
                 } else{ /*if red wins*/
                     Text score = new Text("Red Wins!");
                     score.setFill(Color.RED);
-                    score.setFont(Font.font("Arial", FontWeight.BOLD, 24));
-                    score.setStyle("-fx-stroke: black; -fx-stroke-width: 2");
+                    score.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+                    score.setStyle("-fx-stroke: black; -fx-stroke-width: 1");
                     popUp2.getChildren().clear(); /*get rid of the last game's result*/
                     popUp2.getChildren().add(score);
                     score.setLayoutX(290);
